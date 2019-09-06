30d9a1459703d616b60f6d3bd4797f0deec77c2d
michaelromanov <michael.alexander.romanov@gmail.com>
2019-09-06 09:02:20 -0600

    starting component state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component }  from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
      <div className='App'>
-       <h1>Hello </h1>
+       <Navbar />
      </div>
     );  
-  } 
+  }  
 }
 
 export default App;
