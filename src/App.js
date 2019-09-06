0ee5790a59bf8cf1d66f98926dcc528e498188d5
michaelromanov <michael.alexander.romanov@gmail.com>
2019-09-06 01:49:04 -0600

    upd

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello from React</h1> 
-      </div>
-    ); 
-  }
+     <div className='App'>
+       <h1>Hello </h1>
+     </div>
+    );  
+  } 
 }
 
 export default App;
