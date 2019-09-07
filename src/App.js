088a9506019bce5c1eef27a7d0035cda55459a4b
michaelromanov <michael.alexander.romanov@gmail.com>
2019-09-07 03:45:23 -0600

    upd

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ class App extends Component {
     return (
      <div className='App'>
        <Navbar />
-       <Users /> 
+       <div className="container">
+        <Users /> 
+       </div>
      </div>
     );  
-  }  
+  }   
 }
 
 export default App;
