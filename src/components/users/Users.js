a395c8ff2b81aa3288b0586c8024ad03d58b2488
michaelromanov <michael.alexander.romanov@gmail.com>
2019-09-07 01:17:22 -0600

    upd

---
+++ src/components/users/Users.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Users extends Component {
+  state = {
+    users: [
+    { id: '1', 
+      login: 'mojobmo', 
+      avatar_url: 'htpps://avatars0.githubusercontent.com/u/1?v=4', 
+      html_url: 'https://github.com/mojombo'
+    }, 
+    users: {
+      id: '', 
+      login: '', 
+      avatar_url: 'htpps://avatars0.githubusercontent.com/u/2?v=4', 
+      html_url: 'https://github.com/defunkt'
+    }, 
+    users: {
+      id: '', 
+      login: '', 
+      avatar_url: 'htpps://avatars0.githubusercontent.com/u/3?v=4', 
+      html_url: 'https://github.com/'
+    }
+  ]
+}
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Users
