d72fb668015fc0201b024f32ad35e9dc734bb1aa
michaelromanov <michael.alexander.romanov@gmail.com>
2019-09-10 23:58:30 -0600

    loader.js added

---
+++ loader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class loader extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default loader
